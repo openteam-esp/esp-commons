524b3ecdfee6f50057e715abcbd6edd548042e70
Dmitry Lihachev <lda@openteam.ru>
2012-02-06 12:27:28 +0700

    значения настроек должны быть необязательными (чтобы rake мог нормально работать)

--- lib/esp-commons/engine.rb
+++ lib/esp-commons/engine.rb
@@ -10,9 +10,9 @@ module EspCommons
       Settings.read(Rails.root.join('config', 'settings.yml')) if Rails.root.join('config', 'settings.yml').exist?
       Settings.defaults Settings.extract!(Rails.env)[Rails.env] || {}
       Settings.extract!(:test, :development, :production)
-      Settings.define 'app.secret',       :env_var => 'APP_SECRET',     :required => true
-      Settings.define 'errors.key',       :env_var => 'ERRORS_KEY',     :required => Rails.env.production?
-      Settings.define 'errors.url',       :env_var => 'ERRORS_URL',     :required => Rails.env.production?
+      Settings.define 'app.secret',       :env_var => 'APP_SECRET'
+      Settings.define 'errors.key',       :env_var => 'ERRORS_KEY'
+      Settings.define 'errors.url',       :env_var => 'ERRORS_URL'
       Settings.define 'solr.url',         :env_var => 'SOLR_URL'
       Settings.define 'sso.url',          :env_var => 'SSO_URL'
       Settings.define 'sso_key',          :env_var => 'SSO_KEY'
@@ -22,7 +22,6 @@ module EspCommons
       Settings.define 'news.url',         :env_var => 'NEWS_URL'
       Settings.define 'vfs.url',          :env_var => 'VFS_URL'
       Settings.define 'appeals.url',      :env_var => 'APPEALS_URL'
-      Settings.resolve!
     end
 
     config.before_configuration do
