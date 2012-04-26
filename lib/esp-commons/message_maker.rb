6a2fb4647de294af5549764619291c8170054576
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-04-26 11:21:01 +0700

    pass queue name to MessageMaker#make_message

--- lib/esp-commons/message_maker.rb
+++ lib/esp-commons/message_maker.rb
@@ -1,14 +1,14 @@
 require 'bunny'
 
 class MessageMaker
-  def self.make_message(routing_key, message)
+  def self.make_message(queue_name, routing_key, message)
     amqp_client = Bunny.new(Settings['amqp.url'], :logging => false)
 
     amqp_client.start
 
     exchange = amqp_client.exchange('esp.exchange', :type => :topic)
 
-    queue = amqp_client.queue('esp.queue', :durable => true)
+    queue = amqp_client.queue(queue_name, :durable => true)
 
     exchange.publish(message, :key => routing_key, :persistent => true)
 
