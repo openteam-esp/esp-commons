6a0aef66f3ffd03152f5a74144305da4df93e97e
Dmitry Lihachev <lda@openteam.ru>
2012-12-17 10:11:05 +0700

    allow working without rabbitmq

--- lib/esp-commons/libraries/message_maker.rb
+++ lib/esp-commons/libraries/message_maker.rb
@@ -2,18 +2,24 @@ require 'bunny'
 
 class MessageMaker
   def self.make_message(queue_name, routing_key, *message)
-    amqp_client = Bunny.new(Settings['amqp.url'], :logging => false)
+    if Settings['amqp.url']
+      amqp_client = Bunny.new(Settings['amqp.url'], :logging => false)
+      amqp_client.start
 
-    amqp_client.start
+      exchange = amqp_client.exchange(queue_name, :type => :topic)
 
-    exchange = amqp_client.exchange(queue_name, :type => :topic)
+      queue = amqp_client.queue(queue_name, :durable => true)
+      queue.bind(exchange, :key => '*')
 
-    queue = amqp_client.queue(queue_name, :durable => true)
+      exchange.publish(message.as_json.to_json, :key => routing_key, :persistent => true)
 
-    queue.bind(exchange, :key => '*')
-
-    exchange.publish(message.as_json.to_json, :key => routing_key, :persistent => true)
-
-    amqp_client.stop
+      amqp_client.stop
+    else
+      if Rails.env.production?
+        raise 'Define amqp.url in config/settings.yml'
+      else
+        ActiveSupport::Deprecation.warn 'Define amqp.url in config/settings.yml'
+      end
+    end
   end
 end
